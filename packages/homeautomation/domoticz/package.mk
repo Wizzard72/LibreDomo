cf0eaa3488f5a9f89fe206efce74815e02bae32c
Wizzard72 <paulslats@gmail.com>
2020-10-13 13:01:43 +0200

    Update package.mk

--- packages/homeautomation/domoticz/package.mk
+++ packages/homeautomation/domoticz/package.mk
@@ -1,18 +1,18 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
-PKG_NAME="https://mosquitto.org/files/source/mosquitto-1.6.12.tar.gz"
-PKG_VERSION="1.6.12"
+PKG_NAME="domoticz"
+PKG_VERSION="2020.1"
 PKG_SHA256="[sha256 hash of the source file, downloaded from PKG_URL]"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://mosquitto.org"
-PKG_URL="https://mosquitto.org/files/source/mosquitto-1.6.12.tar.gz"
+PKG_SITE="https://www.domoticz.com"
+PKG_URL="https://releases.domoticz.com/releases/release/domoticz_linux_armv7l.tgz"
 # PKG_MAINTAINER="John Doe" # Full name or forum/GitHub nickname, if you want to be identified as the addon maintainer
-PKG_DEPENDS_TARGET="[build system dependencies, e.g. toolchain zlib openssl]"
-PKG_SECTION="[location under packages, e.g. database]"
-PKG_SHORTDESC="[short description of the package, e.g. libexample: library to calculate examples]"
-PKG_LONGDESC="[long description of the package, often taken from the package/project website, e.g. libexample: this project is created to calculate examples for x and y, with maximum efficiency and fewer errors]"
+PKG_DEPENDS_TARGET="boost toolchain zlib openssl]"
+PKG_SECTION="homeautomation"
+PKG_SHORTDESC="Domoticz is a Home Automation System that lets you monitor and configure various devices like: Lights, Switches, various sensors/meters like Temperature, Rain, Wind, UV, Electra, Gas, Water and much more."
+PKG_LONGDESC="Domoticz is a Home Automation System that lets you monitor and configure various devices like: Lights, Switches, various sensors/meters like Temperature, Rain, Wind, UV, Electra, Gas, Water and much more."
 # PKG_TOOLCHAIN="auto"
 
 #PKG_CMAKE_OPTS_TARGET="-DWITH_EXAMPLE_PATH=/storage/.example
