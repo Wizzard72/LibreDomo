9b947d37ddfe2556d4d45dc6bebd12e0d47441f4
Wizzard72 <paulslats@gmail.com>
2020-10-15 21:57:56 +0200

    Update package.mk

--- packages/homeautomation/mosquitto/package.mk
+++ packages/homeautomation/mosquitto/package.mk
@@ -7,12 +7,12 @@ PKG_SHA256="[sha256 hash of the source file, downloaded from PKG_URL]"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://mosquitto.org"
-PKG_URL="https://mosquitto.org/files/source/mosquitto-1.6.12.tar.gz"
+PKG_URL="https://mosquitto.org/files/source/$PKG_NAME-$PKG_VERSION.tar.gz"
 # PKG_MAINTAINER="John Doe" # Full name or forum/GitHub nickname, if you want to be identified as the addon maintainer
-PKG_DEPENDS_TARGET="[build system dependencies, e.g. toolchain zlib openssl]"
-PKG_SECTION="[location under packages, e.g. database]"
-PKG_SHORTDESC="[short description of the package, e.g. libexample: library to calculate examples]"
-PKG_LONGDESC="[long description of the package, often taken from the package/project website, e.g. libexample: this project is created to calculate examples for x and y, with maximum efficiency and fewer errors]"
+PKG_DEPENDS_TARGET="toolchain]"
+PKG_SECTION="homeautomation"
+PKG_SHORTDESC="This is a message broker that supports version 5.0, 3.1.1, and 3.1 of the MQTT protocol."
+PKG_LONGDESC=This is a message broker that supports version 5.0, 3.1.1, and 3.1 of the MQTT protocol. MQTT provides a method of carrying out messaging using a publish/subscribe model. It is lightweight, both in terms of bandwidth usage and ease of implementation. This makes it particularly useful at the edge of the network where a sensor or other simple device may be implemented using an arduino for example."
 # PKG_TOOLCHAIN="auto"
 
 #PKG_CMAKE_OPTS_TARGET="-DWITH_EXAMPLE_PATH=/storage/.example
