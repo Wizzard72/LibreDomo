15d2adf9dd4b2a6d5f27b738b6b47d2281c5dbcd
Wizzard72 <paulslats@gmail.com>
2020-10-16 16:15:18 +0200

    Create package.mk

---
+++ packages/virtual/homeautomation/package.mk
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="homeautomation"
+PKG_VERSION=""
+PKG_LICENSE="GPL"
+PKG_SITE="https://libredomo.todo"
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain $HOMEAUTOMATION"
+PKG_SECTION="virtual"
+PKG_LONGDESC="Home Automation: Metapackage"
+
+if [ "$HOMEAUTOMATION" = "domoticz" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET"
+
+# some python stuff needed for various addons
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET Pillow \
+                                          simplejson \
+                                          pycryptodome"
+
+# other packages
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET xmlstarlet"
+
+
+fi
