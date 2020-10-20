6c7706643524aca106a8a082a63a48d8097fe1a4
Wizzard72 <paulslats@gmail.com>
2020-10-20 11:50:31 +0200

    Update package.mk

--- packages/devel/popt/package.mk
+++ packages/devel/popt/package.mk
@@ -1,13 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2016-2020 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2020-present Team LibreDOMO (https://libredomo.todo)
 
 PKG_NAME="popt"
-PKG_VERSION="1.16"
-PKG_SHA256="13c34f4af1d389668dd2018a816d4a526aaede6a37f5de80d7be1eec1564f820"
+PKG_VERSION="1.18"
+PKG_SHA256="5159bc03a20b28ce363aa96765f37df99ea4d8850b1ece17d1e6ad5c24fdc5d1"
 PKG_LICENSE="GPL"
 PKG_SITE="http://rpm5.org/files/popt/"
-PKG_URL="http://rpm5.org/files/popt/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="http://ftp.rpm.org/popt/releases/popt-1.x/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="The popt library exists essentially for parsing command-line options."
 
