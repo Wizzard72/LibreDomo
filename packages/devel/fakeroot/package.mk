13d70898140d79f904d45745997636b6d75e7b19
Wizzard72 <paulslats@gmail.com>
2020-10-20 09:59:37 +0200

    Update package.mk

--- packages/devel/fakeroot/package.mk
+++ packages/devel/fakeroot/package.mk
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2019-2020 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2020-present Team LibreDOMO (https://libredomo.todo)
 
 PKG_NAME="fakeroot"
 PKG_VERSION="1.25.3"
@@ -10,5 +11,6 @@ PKG_SITE="http://freshmeat.sourceforge.net/projects/fakeroot"
 PKG_URL="http://ftp.debian.org/debian/pool/main/f/fakeroot/${PKG_NAME}_${PKG_VERSION}.orig.tar.gz"
 PKG_DEPENDS_HOST="ccache:host libcap:host"
 PKG_LONGDESC="fakeroot provides a fake root environment by means of LD_PRELOAD and SYSV IPC (or TCP) trickery."
+PKG_TOOLCHAIN="auto"
 
 PKG_CONFIGURE_OPTS_HOST="--with-gnu-ld"
