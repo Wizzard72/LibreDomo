035381c6ed73b6a8b33816ad1df5e77d189d84ca
Wizzard72 <paulslats@gmail.com>
2020-10-20 14:09:42 +0200

    Update package.mk

--- packages/homeautomation/openzwave/package.mk
+++ packages/homeautomation/openzwave/package.mk
@@ -15,6 +15,10 @@ PKG_SHORTDESC="Our goal is to create free software library that interfaces with 
 PKG_LONGDESC="Our goal is to create free software library that interfaces with Z-Wave controllers, allowing anyone to create applications to control devices on a Z-Wave network, without requiring in-depth knowledge of the Z-Wave protocol. OpenZWave is not about creating Z-Wave devices (nodes). The project consists of the main library, written in C++ and wrappers and supporting projects, to interface different languages and protocol(s)."
 # PKG_TOOLCHAIN="auto"
 
+pre_make_target() {
+  export CFLAGS=""
+}
+
 #PKG_CMAKE_OPTS_TARGET="-DWITH_EXAMPLE_PATH=/storage/.example
 #                      "
 
